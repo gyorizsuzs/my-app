80de640b1c8844bb2601ffd369480728575fb012
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-09-09 15:44:33 +0200

    base app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Pizza from "./components/Pizza/Pizza";
 
 function App() {
+  const pizzas = ["Diavola", "Kolbaszos Rajas", "Bubbletini"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1>sziumiu</h1>
+          {pizzas.map((pizza) => (
+            <Pizza name={pizza} />
+          ))}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Pizza/Pizza.js
@@ -0,0 +1,11 @@
+import "./Pizza.css";
+
+function Pizza({ name }) {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default Pizza;
